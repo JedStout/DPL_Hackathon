bc51923d61ab894ffdc04eed4065208269145802
Jed Stout <jedstout@gmail.com>
2017-09-09 11:08:44 -0600

    user controller

--- twitterGram/config/routes.rb
+++ twitterGram/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'user/email:string'
+
+  get 'user/password:string'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "#index"
+  root to: "post#index"
 end
